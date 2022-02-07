9f7a25f5a74f57a52b6b5c30dd2c066647a80b41
Albarion <damianassmann+github@gmail.com>
2022-02-07 21:18:27 +0100

    redo

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1434,7 +1434,7 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 // @advi3++: The bed dimensions of Wanhao i3 Plus is (officially) 200x200x180
 #define X_MIN_POS 0
-#define Y_MIN_POS -60
+#define Y_MIN_POS -6
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
